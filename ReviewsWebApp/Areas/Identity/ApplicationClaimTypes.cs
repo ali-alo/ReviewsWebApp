daedd0157585d10612b03090ebbfc0e68cc6e467
Ali <alialo3888@gmail.com>
2023-09-02 18:34:44 +0500

    Add FirstName claim and scaffold register pages

---
+++ ReviewsWebApp/Areas/Identity/ApplicationClaimTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace ReviewsWebApp.Areas.Identity
+{
+    public static class ApplicationClaimTypes
+    {
+        public const string FirstName = "firstname";
+    }
+}
--- ReviewsWebApp/Data/ApplicationUser.cs
+++ ReviewsWebApp/Data/ApplicationUser.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using ReviewsWebApp.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace ReviewsWebApp.Data
 {
     public class ApplicationUser : IdentityUser
     {
         public List<Review> Reviews { get; set; } = new List<Review>();
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [MaxLength(50)]
+        public string LastName { get; set; }
     }
 }
